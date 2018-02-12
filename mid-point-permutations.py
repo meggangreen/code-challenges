c1053ccc3c2c9aabbd43799d7fe8247b30df8ab5
Meggan M Green <github@meggangreen.com>
2018-02-12 21:55:55 +0000

    final commit

--- mid-point-permutations.py
+++ mid-point-permutations.py
@@ -19,6 +19,9 @@ So, The middle term is "bac".:
 def find_all_permutations(s):
     """ Returns a list of all possible permutations.
 
+        >>> find_all_permutations('bac')
+        ['bac', 'bca', 'abc', 'acb', 'cba', 'cab']
+
     """
 
     if len(s) == 0:
@@ -33,22 +36,28 @@ def find_all_permutations(s):
         current = s[i]
         remaining = s[:i] + s[i+1:]
 
-        for p in find_all_permutations(remaining):
-            permutations.append(current + p)
+        for permutation in find_all_permutations(remaining):
+            permutations.append(current + permutation)
 
     return permutations
 
 
-def find_middle_permutation(permutations):
+def find_middle_permutation(s):
     """ Returns the string located at the midpoint of the sorted list.
+
+        >>> find_middle_permutation('bac')
+        'bac'
 
     """
     from math import ceil
 
-    if len(permutations) == 0:
+
+    if len(s) == 0:
         return None
 
-    if len(permutations) == 1:
-        return permutations[0]
+    if len(s) == 1:
+        return s
 
-    return permutations[ceil(len(sorted(permutations))/2.0)-1]
+    permutations = find_all_permutations(s)
+
+    return sorted(permutations)[int(ceil(len(permutations))/2.0)-1]
