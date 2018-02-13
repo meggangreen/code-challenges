e4c96b0e70438f6a607568d30ba40342000be01e
Meggan M Green <github@meggangreen.com>
2018-02-13 20:09:48 +0000

    @wip building out tree

--- management_tree.py
+++ management_tree.py
@@ -36,22 +36,52 @@ mgr_rpt_pairs = [('Benji', 'Cherise'),
 #           ]),
 #       ])
 
+from collections import deque
+
 class Node(object):
 
-    def __init__(self, name=None):
+    def __init__(self, name):  # name as string
         self.name = name
         self.reportees = []
 
     def __repr__(self):
         return "<Node {}>".format(self.name)
 
-    def add_reportee(self, reportee=None):
-        if reportee:
-            self.reportees.append(Node(reportee))
+    def add_reportee(self, reportee):  # reportee as string or Node
+        if type(reportee) == str:
+            reportee = Node(reportee)
+        self.reportees.append(reportee)
 
 
-def create_management_tree(mgr_rpt_pairs):
-    pass
+class Tree(object):
+
+    def __init__(self, root):  # root as Node
+        self.root = root
+
+    def __repr__(self):
+        return "<Tree with root {}".format(self.root)
+
+    def build_tree(self, employees):  # employees as dictionary
+        pass
+
+
+def create_management_tree(mgr_rpt_pairs):  # root as str; employees as dict
+
+    # get employees and root
+    employees = create_employees(mgr_rpt_pairs)
+    root = get_management_root(employees)
+
+    # start tree
+    root_node = Node(root)
+    tree = Tree(root_node)
+
+    # breadth-first traversal of dictionary to make tree
+    emp_queue = deque()
+
+    for rpt in employees[root]:
+        root_node.add_reportee(rpt)
+
+
 
 def create_employees(mgr_rpt_pairs):
 
@@ -66,7 +96,7 @@ def create_employees(mgr_rpt_pairs):
 
 
 def get_management_root(employees):
-    """ return root node and employees dict for tree assembly """
+    """ return root for tree assembly """
 
     # get set of all reportees (sometimes people have more than 1 boss)
     reportees_set = set()
@@ -77,5 +107,5 @@ def get_management_root(employees):
 
     root = (managers_set - reportees_set).pop()
 
-    return (root, employees)
+    return root
 
