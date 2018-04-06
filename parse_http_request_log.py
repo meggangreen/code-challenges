5605abe6d568b7db38b796b28f6eb7d782a1a260
Meggan M Green <github@meggangreen.com>
2018-04-06 14:38:45 -0700

    parse http log

--- parse_http_request_log.py
+++ parse_http_request_log.py
@@ -1,36 +1,33 @@
 """
-Write a script that, given a web server log file, returns the 10 most frequently
-requested objects and their cumulative bytes transferred. Only include GET
-requests with Successful (HTTP 2xx) responses. Resolve ties however you’d like.
+Given a web server log file, return the 10 most frequently requested files and
+their cumulative bytes transferred. Only include GET requests with HTTP 2xx
+responses. Resolve ties by file name.
 
 Log format:
-- request date, time, and time zone
-- request line from the client
-- HTTP status code returned to the client
-- size (in bytes) of the returned object
+- request timestamp
+- request line from client
+- HTTP status code
+- size of the returned object
 
 Given this input data:
-```
-[01/Aug/1995:00:54:59 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511
-[01/Aug/1995:00:55:04 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635
-[01/Aug/1995:00:55:06 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 403 298
-[01/Aug/1995:00:55:09 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635
-[01/Aug/1995:00:55:18 -0400] "GET /images/opf-logo.gif HTTP/1.0" 200 32511
-[01/Aug/1995:00:56:52 -0400] "GET /images/ksclogosmall.gif HTTP/1.0" 200 3635
-```
+
+    [29/Jan/1776:00:54:59 -0400] "GET /img/filename1.gif HTTP/1.0" 200 32511
+    [29/Jan/1776:00:55:04 -0400] "GET /img/filename2.gif HTTP/1.0" 200 3635
+    [29/Jan/1776:00:55:06 -0400] "GET /img/filename2.gif HTTP/1.0" 403 298
+    [29/Jan/1776:00:55:09 -0400] "GET /img/filename2.gif HTTP/1.0" 200 3635
+    [29/Jan/1776:00:55:18 -0400] "GET /img/filename1.gif HTTP/1.0" 200 32511
+    [29/Jan/1776:00:56:52 -0400] "GET /img/filename2.gif HTTP/1.0" 200 3635
 
 The result should be:
-```
-/images/ksclogosmall.gif 10905
-/images/opf-logo.gif 65022
-```
 
-(You may tweak the output format as you'd like)
+    /img/filename2.gif 10905
+    /img/filename1.gif 65022
+
 """
 
 def get_most_requested(log_file):
-    """ Prints the file path and cumulative bytes transferred to the command
-        line in descending order of times requested.
+    """ Prints the file path and cumulative bytes transferred in descending
+        order of request quantity.
 
     """
 
