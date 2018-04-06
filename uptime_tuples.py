ffc2fb6321c49acc591468fb2e25abfc2ad239b8
Meggan M Green <github@meggangreen.com>
2018-04-06 14:55:49 -0700

    end early for 100pct down

--- uptime_tuples.py
+++ uptime_tuples.py
@@ -13,4 +13,23 @@
 # Optimized answer uses in O(nlogn) time and space.
 #
 
+def get_total_uptime(network_t):
+    """ Returns one list of tuples representing overall network uptime. """
 
+    total_uptime = []
+    end_time = network_t[0][-1][0]
+
+    ideal_uptime = [(end_time, 1)]  # Ideal is up (1) for entire time
+
+    for device_t in network_t:
+        # Exit this device, and possibly this function, early
+        if len(device_t) == 1:
+            if device_t == [(end_time, 0)]:  # Device was down 100%
+                return device_t
+            else:
+                continue  # Device was up 100%; skip it
+        start_time = 0
+        for t in device_t:
+
+
+    return total_uptime
