009632a861b58bcc6fa93d25a71c1a43204cdac6
Meggan M Green <github@meggangreen.com>
2018-05-03 02:04:49 +0000

    stringified blocks of digits

--- nth_root.py
+++ nth_root.py
@@ -5,14 +5,21 @@
 # Given y = x**n, find x from y & n ... without using the very simple x = y**(1/n)
 # IE: write the min-max-avg method a calculator would use to solve a root
 
-def calc_root(radicand, index):
+def calc_root_shifting_point(radicand, index):
 
-    if radicand < 1 or index < 0:
+    if radicand < 0 or index < 1:
         return None
 
     if index == 1:
         return radicand
 
-    radicand_s = str(radicand)
-    blocks[1] = radicand_s[]
-    blocks = [num[i:i+index:index]]
+    # Set up blocks of numbers (possible helper func)
+    rad_s = str(radicand)
+    dec = rad_s.find('.')
+    rad_s = rad_s[:dec] + rad_s[dec+1:]
+    rem = len(rad_s) % index if len(rad_s) % index != 0 else index
+    blocks = ['0' * index] * len(rad_s) // index + 1
+    blocks[0] = [:rem]
+    blocks[1:] = [rad_s[j:j+index] for j in range(rem, len(rad_s), index)]
+
+
